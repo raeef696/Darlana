7f3a836494f3417365f6f3e423b7272944534c06
raeef696 <75561536+raeef696@users.noreply.github.com>
2022-04-23 15:28:52 -1100

    new

---
+++ assest/js/anmashin.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+// let section = document.querySelector(".section-cont");
+
+// window.onscroll = function () {
+//     if (window.scrollY >= section.offsetTop) {
+//         $(section).addClass("slide-top");
+//     }
+// }
